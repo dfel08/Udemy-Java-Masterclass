4b15f9905063e8292ad1bd97fc146c8e2d5f82b4
dfel08 <dfellows68@gmail.com>
2020-06-12 19:47:33 -0700

    Finish Strings

--- Section 3/Strings/src/academy/learnprogramming/Main.java
+++ Section 3/Strings/src/academy/learnprogramming/Main.java
@@ -3,6 +3,29 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2020";
+        System.out.println("myString is equal to " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println("numberString is equal to " + numberString);
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("lastString is equal to " + lastString);
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println("lastString is equal to " + lastString);
     }
 }
