fbd6085d3113945406b73bced1a70c603c7d0611
dfel08 <dfellows68@gmail.com>
2020-06-14 15:51:55 -0700

    Finish video 33

--- Section 3/Operators/src/academy/learnprogramming/Main.java
+++ Section 3/Operators/src/academy/learnprogramming/Main.java
@@ -1,5 +1,7 @@
 package academy.learnprogramming;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -24,6 +26,25 @@ public class Main {
         //result = result + 1;
         result++; //1 + 1 = 2
         System.out.println("1 + 1 = " + result);
+
+        result--; //2 - 1 = 1
+        System.out.println("2 - 1 = " + result);
+
+        //result = result +2;
+        result += 2; //1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        //result = result * 10;
+        result *= 10; //3 * 10 = 30
+        System.out.println("3 * 10 = " + result);
+
+        //result = result / 3;
+        result /= 3; //30 / 3 = 10
+        System.out.println("30 / 3 = " + result);
+
+        //result = result - 2;
+        result -= 2; //10 - 2 = 8
+        System.out.println("10 - 2 = " + result);
 
     }
 }
