3f5d973a2d6bcb9e7ef7e5b775c509541d05deb4
dfel08 <dfellows68@gmail.com>
2020-06-14 16:06:20 -0700

    Finish video 34

--- Section 3/Operators/src/academy/learnprogramming/Main.java
+++ Section 3/Operators/src/academy/learnprogramming/Main.java
@@ -1,7 +1,5 @@
 package academy.learnprogramming;
 
-import org.w3c.dom.ls.LSOutput;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -46,5 +44,10 @@ public class Main {
         result -= 2; //10 - 2 = 8
         System.out.println("10 - 2 = " + result);
 
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
     }
 }
