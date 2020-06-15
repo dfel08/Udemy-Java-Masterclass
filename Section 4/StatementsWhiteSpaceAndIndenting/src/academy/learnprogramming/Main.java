338856d50860e7f8b678b7f7dca1c6f5b29d94f5
dfel08 <dfellows68@gmail.com>
2020-06-15 13:20:35 -0700

    Finish StatementsWhiteSpaceAndIndenting

--- Section 4/StatementsWhiteSpaceAndIndenting/src/academy/learnprogramming/Main.java
+++ Section 4/StatementsWhiteSpaceAndIndenting/src/academy/learnprogramming/Main.java
@@ -3,6 +3,20 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Statements
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is " +
+                " another" +
+                " still more.");
+
+        int anotherVariable = 50;
+        anotherVariable--;
+        System.out.println("This is another one");
+
+        //White space is the space in between your expressions, operators and so forth
     }
 }
