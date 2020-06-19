5e46baf53cab2b4bcf64a3b951ea72cc2e71402d
dfel08 <dfellows68@gmail.com>
2020-06-19 17:11:27 -0500

    Finish TeenNumberChecker

--- Section 4/TeenNumberChecker/src/TeenNumberChecker.java
+++ Section 4/TeenNumberChecker/src/TeenNumberChecker.java
@@ -1,2 +1,16 @@
 public class TeenNumberChecker {
+    public static boolean hasTeen (int ageOne, int ageTwo, int ageThree) {
+        if (isTeen(ageOne) == true || isTeen(ageTwo) == true || isTeen(ageThree) == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static boolean isTeen (int age) {
+        if (age > 12 && age < 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
