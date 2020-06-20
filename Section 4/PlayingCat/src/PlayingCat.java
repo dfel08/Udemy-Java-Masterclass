0720fd674c9b491fa877bbe627ec075d99a54457
dfel08 <dfellows68@gmail.com>
2020-06-20 12:10:58 -0500

    Finish PlayingCat

--- Section 4/PlayingCat/src/PlayingCat.java
+++ Section 4/PlayingCat/src/PlayingCat.java
@@ -1,2 +1,11 @@
 public class PlayingCat {
+    public static boolean isCatPlaying (boolean summer, int temperature) {
+        if (temperature <= 35 && temperature >= 25 && summer == false) {
+            return true;
+        } else if (temperature <= 45 && temperature >= 25 && summer == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
