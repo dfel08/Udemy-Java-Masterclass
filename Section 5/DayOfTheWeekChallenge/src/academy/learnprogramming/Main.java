4a52603cb0fe6116a85b5844ea4206789bb10376
dfel08 <dfellows68@gmail.com>
2020-06-20 17:24:04 -0500

    Finish DayOfWeekChallenge:

--- Section 5/DayOfTheWeekChallenge/src/academy/learnprogramming/Main.java
+++ Section 5/DayOfTheWeekChallenge/src/academy/learnprogramming/Main.java
@@ -3,8 +3,37 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	    public static void printDayOfWeek (int day) {
-	        switch
+        printDayOfWeek(0);
+        printDayOfWeek(5);
+        printDayOfWeek(12);
+    }
+
+    public static void printDayOfWeek (int day) {
+        switch (day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
         }
     }
 }
