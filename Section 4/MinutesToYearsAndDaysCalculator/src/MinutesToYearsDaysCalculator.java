989468b5b1c6537306f3705ead359fc7e27a1c38
dfel08 <dfellows68@gmail.com>
2020-06-20 11:49:22 -0500

    Finish MinutesToYearsDaysCalculator

--- Section 4/MinutesToYearsAndDaysCalculator/src/MinutesToYearsDaysCalculator.java
+++ Section 4/MinutesToYearsAndDaysCalculator/src/MinutesToYearsDaysCalculator.java
@@ -1,2 +1,15 @@
 public class MinutesToYearsDaysCalculator {
+    public static void printYearsAndDays (long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long hours = minutes / 60;
+            long days = hours / 24;
+            long remainingMinutes = minutes % 60;
+            long years = days / 365;
+            long remainingHours = hours % 24;
+            long remainingDays = days % 365;
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+        }
+    }
 }
