f45f28c7e4ac7a4f8bcf79efed0d72e03a819aa5
dfel08 <dfellows68@gmail.com>
2020-06-20 11:02:05 -0500

    Finish SecondsAndMinutesChallenge

--- Section 4/SecondsAndMinutesChallenge/src/academy/learnprogramming/Main.java
+++ Section 4/SecondsAndMinutesChallenge/src/academy/learnprogramming/Main.java
@@ -3,8 +3,30 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        getDurationString(63, 30);
+        getDurationString(3675);
     }
 
-    public static int
+    public static String getDurationString (int minutes, int seconds) {
+        if ((minutes >= 0) && (seconds >= 0 && seconds <= 59)) {
+            int hours = minutes / 60;
+            int newMinutes = minutes % 60;
+            System.out.println(hours + "h " + newMinutes + "m " + seconds + "s");
+            return (hours + "h " + newMinutes + "m " + seconds + "s");
+        } else {
+            System.out.println("Invalid value");
+            return "Invalid Value";
+        }
+    }
+
+    public static String getDurationString (int seconds) {
+        if (seconds >= 0) {
+            int minutes = seconds / 60;
+            int newSeconds = seconds % 60;
+            return getDurationString(minutes, newSeconds);
+        } else {
+            System.out.println("Invalid value");
+            return "Invalid Value";
+        }
+    }
 }
