d24e7247978886ce5b66fdd81bab9030bd23764e
dfel08 <dfellows68@gmail.com>
2020-06-21 13:16:01 -0500

    Finish SumOdd

--- Section 5/SumOdd/src/SumOddRange.java
+++ Section 5/SumOdd/src/SumOddRange.java
@@ -1,2 +1,26 @@
 public class SumOddRange {
+    public static boolean isOdd (int number) {
+        if (number > 0) {
+            if (number % 2 != 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static int sumOdd (int start, int end) {
+        int sum = 0;
+        if (start <= end && end > 0 && start > 0) {
+            for (start = start; start <= end; start++) {
+                if (isOdd(start)) {
+                    sum += start;
+                }
+            }
+        } else {
+            return -1;
+        } return sum;
+    }
 }
