1334ceda710df50f38c29412a946459ccae20179
dfel08 <dfellows68@gmail.com>
2020-06-22 14:22:09 -0500

    Finish FactorPrinter

---
+++ Section 5/AllFactors/src/FactorPrinter.java
@@ -0,0 +1,15 @@
+public abstract class FactorPrinter {
+    public static void printFactors (int number) {
+        if (number < 1) {
+            System.out.println("Invalid Value");
+        } else {
+            int factor = 1;
+            while (factor <= number) {
+                if (number % factor == 0) {
+                    System.out.println(factor);
+                }
+                factor++;
+            }
+        }
+    }
+}
