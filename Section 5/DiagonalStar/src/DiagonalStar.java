14cd77304bed2fe7ca307129de903aa15f4cc053
dfel08 <dfellows68@gmail.com>
2020-06-22 15:45:17 -0500

    Finish DiagonalStar

--- Section 5/DiagonalStar/src/DiagonalStar.java
+++ Section 5/DiagonalStar/src/DiagonalStar.java
@@ -1,2 +1,19 @@
 public class DiagonalStar {
+    public static void printSquareStar(int number) {
+        if(number < 5) {
+            System.out.println("Invalid Value");
+        } else {
+            for(int row = 1; row <= number; row++) {
+                for(int column = 1; column <= number; column++) {
+                    if(column == number) {
+                        System.out.println("*");
+                    } else if((row == 1) || (row == number) || (column == 1) || (row == column) || (row == (number - column + 1))) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+        }
+    }
 }
