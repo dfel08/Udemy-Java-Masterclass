b24c55cfff82608ea0bbb7189ed11185e0b9391a
dfel08 <dfellows68@gmail.com>
2020-06-22 13:00:48 -0500

    Finish SharedDigit

--- Section 5/SharedDigit/src/SharedDigit.java
+++ Section 5/SharedDigit/src/SharedDigit.java
@@ -1,2 +1,17 @@
 public class SharedDigit {
+    public static boolean hasSharedDigit (int numOne, int numTwo) {
+        if ((numOne < 10 || numOne > 99) || (numTwo < 10 || numTwo > 99)) {
+            return false;
+        } else {
+            int numOneDigitOne = numOne % 10;
+            int numOneDigitTwo = numOne / 10;
+            int numTwoDigitOne = numTwo % 10;
+            int numTwoDigitTwo = numTwo / 10;
+            if ((numOneDigitOne == numTwoDigitOne || numOneDigitOne == numTwoDigitTwo) || (numOneDigitTwo == numTwoDigitOne || numOneDigitTwo == numTwoDigitTwo)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
