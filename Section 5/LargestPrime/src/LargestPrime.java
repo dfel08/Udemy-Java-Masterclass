c8ea03fc54094ed27675fec58aad42cfffb41e72
dfel08 <dfellows68@gmail.com>
2020-06-22 15:35:37 -0500

    Finish LargestPrime

--- Section 5/LargestPrime/src/LargestPrime.java
+++ Section 5/LargestPrime/src/LargestPrime.java
@@ -1,2 +1,23 @@
 public class LargestPrime {
+    public static int getLargestPrime (int number) {
+        if (number <= 1) {
+            return -1;
+        } else {
+            int largestPrime = 2;
+            boolean flag = false;
+
+            for (int i = 2; i <= number; i++) {
+                if (number % i == 0) {
+                    for (int j = 2; j <= i/2; j++) {
+                        if (i % j == 0) {
+                            flag = true;
+                            break;
+                        }
+                    } if (!flag) {
+                        largestPrime = i;
+                    }
+                }
+            } return largestPrime;
+        }
+    }
 }
