bc8f5a6d93b9aed5d2bb224af55904c85f8b1796
dfel08 <dfellows68@gmail.com>
2020-06-22 13:26:43 -0500

    Finish LastDigitChecker

--- Section 5/LastDigitChecker/src/LastDigitChecker.java
+++ Section 5/LastDigitChecker/src/LastDigitChecker.java
@@ -1,2 +1,24 @@
 public class LastDigitChecker {
+    public static boolean hasSameLastDigit (int numOne, int numTwo, int numThree) {
+        if (!isValid(numOne) || !isValid(numTwo) || !isValid(numThree)) {
+            return false;
+        } else {
+            int leftNumOne = numOne % 10;
+            int leftNumTwo = numTwo % 10;
+            int leftNumThree = numThree % 10;
+            if (leftNumOne == leftNumTwo || leftNumOne == leftNumThree || leftNumTwo == leftNumThree) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static boolean isValid (int num) {
+        if (num < 10 || num > 1000) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
