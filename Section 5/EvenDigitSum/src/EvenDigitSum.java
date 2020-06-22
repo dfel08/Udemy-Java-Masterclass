dd3a0925564f25e77e9f45830e4f871dfbd35b20
dfel08 <dfellows68@gmail.com>
2020-06-22 12:13:20 -0500

    Finish EvenDigitSum

--- Section 5/EvenDigitSum/src/EvenDigitSum.java
+++ Section 5/EvenDigitSum/src/EvenDigitSum.java
@@ -1,2 +1,15 @@
 public class EvenDigitSum {
+    public static int getEvenDigitSum (int number) {
+        int sum = 0;
+        if (number < 0) {
+            return -1;
+        } else {
+            while (number != 0) {
+                int lastDigit = number % 10;
+                if (lastDigit % 2 == 0) {
+                    sum += lastDigit;
+                } number /= 10;
+            }
+        } return sum;
+    }
 }
