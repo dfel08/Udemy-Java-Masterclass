c242b01ddd9b33c01a053613b136f9d09b6e4283
dfel08 <dfellows68@gmail.com>
2020-06-22 14:45:45 -0500

    Finish PerfectNumber

--- Section 5/PerfectNumber/src/PerfectNumber.java
+++ Section 5/PerfectNumber/src/PerfectNumber.java
@@ -1,2 +1,17 @@
 public class PerfectNumber {
+    public static boolean isPerfectNumber(int number) {
+        if (number >= 1) {
+            int i = 1, sum = 0;
+            while (i < number) {
+                if (number % i == 0) {
+                    sum = sum + i;
+                }
+                i++;
+            }
+            if (sum == number) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
