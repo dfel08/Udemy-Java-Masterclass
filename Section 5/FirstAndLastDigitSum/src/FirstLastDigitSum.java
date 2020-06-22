6901623616327d978fcfd93555ec34bae8d1fe36
dfel08 <dfellows68@gmail.com>
2020-06-22 11:48:01 -0500

    Finish FirstAndLastDigitSum

--- Section 5/FirstAndLastDigitSum/src/FirstLastDigitSum.java
+++ Section 5/FirstAndLastDigitSum/src/FirstLastDigitSum.java
@@ -1,2 +1,12 @@
 public class FirstLastDigitSum {
+    public static int sumFirstAndLastDigit(int number) {
+        if (number >= 0) {
+            int firstNumber = number % 10;
+            while (number >= 10) {
+                number = number / 10;
+            }
+            return firstNumber + (number % 10);
+        }
+        return -1;
+    }
 }
