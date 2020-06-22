56453cf17165bea39e7ddfb7d42bf5e093406139
dfel08 <dfellows68@gmail.com>
2020-06-22 14:00:28 -0500

    Finish GreatestCommonDivisor

--- Section 5/GreatestCommonDivisor/src/GreatestCommonDivisor.java
+++ Section 5/GreatestCommonDivisor/src/GreatestCommonDivisor.java
@@ -1,2 +1,15 @@
 public class GreatestCommonDivisor {
+    public static int getGreatestCommonDivisor(int first, int second){
+        if(first < 10 || second < 10){
+            return -1;
+        }
+        int greatestCommonDivisor = 0;
+        int i = 1;
+        while(i <= first && i <= second){
+            if(first % i == 0 && second % i == 0)
+                greatestCommonDivisor = i;
+            i++;
+        }
+        return greatestCommonDivisor;
+    }
 }
