d38c57099bb4463596ac88e45eb1250c07a0bddb
dfel08 <dfellows68@gmail.com>
2020-06-22 15:22:36 -0500

    Finish FlourPackProblem

--- Section 5/FlourPackProblem/src/FlourPacker.java
+++ Section 5/FlourPackProblem/src/FlourPacker.java
@@ -1,2 +1,16 @@
 public class FlourPacker {
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        int bigCountKilo = 5 * bigCount;
+        if ((bigCount < 0) || (smallCount < 0) || (goal < 0)) {
+            return false;
+        }
+        if (goal > bigCountKilo + smallCount) {
+            return false;
+        }
+        if (goal % 5 > smallCount) {
+            return false;
+        }
+        return true;
+    }
 }
+
