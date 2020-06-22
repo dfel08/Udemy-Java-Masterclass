9e70537411360a6eecb92a5582d0514cf2c619a7
dfel08 <dfellows68@gmail.com>
2020-06-22 11:31:32 -0500

    Finish NumberPalindrome

--- Section 5/NumberPalindrome/src/NumberPalindrome.java
+++ Section 5/NumberPalindrome/src/NumberPalindrome.java
@@ -1,2 +1,14 @@
 public class NumberPalindrome {
+    public static boolean isPalindrome(int number) {
+        int initialNumber = number;
+        int reverse = 0;
+        while (number != 0) {
+            reverse = number % 10;
+            number /= 10;
+        }
+        if (initialNumber == reverse) {
+            return true;
+        }
+        return false;
+    }
 }
