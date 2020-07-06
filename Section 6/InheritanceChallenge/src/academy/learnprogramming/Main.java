d9652cc208b8765411e28c976eeaa663617d18d9
dfel08 <dfellows68@gmail.com>
2020-07-05 20:46:40 -0500

    Finish InheritanceChallenge

--- Section 6/InheritanceChallenge/src/academy/learnprogramming/Main.java
+++ Section 6/InheritanceChallenge/src/academy/learnprogramming/Main.java
@@ -3,6 +3,20 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Challenge.
+        // Start with a base class which is a Vehicle, then create a Car class that inherits from this base Vehicle class.
+        // Finally, create a third class, which is for a specific type of Car that inherits from the Car class.
+        // You should be able to hand steering, changing gears, and moving (speed in other words).
+        // You will want to decide where to put the appropriate state and behaviors (fields and methods).
+        // As mentioned above, changing gears, increasing/decreasing speed should be included.
+        // For your specific type of vehicle you will want to add something specific for that type of car.
+
+
+        Nissan nissan = new Nissan(36);
+        nissan.steer(45);
+        nissan.accelerate(30);
+        nissan.accelerate(20);
+        nissan.accelerate(-42);
     }
 }
