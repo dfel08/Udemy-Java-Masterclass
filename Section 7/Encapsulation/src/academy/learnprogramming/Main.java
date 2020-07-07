caeea3f77d27e3e99486b7c6b8833a8c414fc6d3
dfel08 <dfellows68@gmail.com>
2020-07-06 19:33:56 -0500

    Halfway through Encapsulation

--- Section 7/Encapsulation/src/academy/learnprogramming/Main.java
+++ Section 7/Encapsulation/src/academy/learnprogramming/Main.java
@@ -3,6 +3,20 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        Player player = new Player();
+//        player.name = "Dan";
+//        player.health = 20;
+//        player.weapon = "AK-47";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+        EnhancedPlayer player = new EnhancedPlayer("Dan", 200, "AR-15");
+        System.out.println("Initial health is " + player.getHealth());
     }
 }
