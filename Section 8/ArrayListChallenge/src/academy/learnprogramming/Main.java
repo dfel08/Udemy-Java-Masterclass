d38e971d73f5400983bfd0ebe91e7fa1990f67eb
dfel08 <dfellows68@gmail.com>
2020-07-09 15:50:18 -0500

    Finish ArrayListChallenge Part 2

--- Section 8/ArrayListChallenge/src/academy/learnprogramming/Main.java
+++ Section 8/ArrayListChallenge/src/academy/learnprogramming/Main.java
@@ -1,6 +1,10 @@
 package academy.learnprogramming;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone("0039 330 4404");
 
     public static void main(String[] args) {
 	    // Create a program that implements a simple mobile phone with the following capabilities.
@@ -16,5 +20,73 @@ public class Main {
         // e.g. no ints, no .get(i) etc
         // MobilePhone should do everything with Contact objects only.
 
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while(!quit) {
+            System.out.println("\nEnter action: (6 to show available actions)");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action) {
+                case 0:
+                    System.out.println("Shutting down...");
+                    quit = true;
+                    break;
+
+                case 1:
+                    mobilePhone.printContacts();
+                    break;
+
+                case 2:
+                    addNewContact();
+                    break;
+
+                case 3:
+                    updateContact();
+                    break;
+
+                case 4:
+                    removeContact();
+                    break;
+
+                case 5:
+                    queryContact();
+                    break;
+
+                case 6:
+                    printActions();
+                    break;
+            }
+        }
+    }
+
+    private static void addNewContact() {
+        System.out.println("Enter new contact name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phone = scanner.nextLine();
+        Contact newContact = Contact.createContact(name, phone);
+        if(mobilePhone.addNewContact(newContact)) {
+            System.out.println("New contact added: name = " + name + ", phone = " + phone);
+        } else {
+            System.out.println("Cannot add, " + name + " already on file");
+        }
+    }
+
+    private static void startPhone() {
+        System.out.println("Starting phone...");
+    }
+
+    private static void printActions() {
+        System.out.println("\nAvailable actions:\npress");
+        System.out.println("0  - to shutdown\n" +
+                           "1  - to print contacts\n" +
+                           "2  - to add a new contact\n" +
+                           "3  - to update existing contact\n" +
+                           "4  - to remove an existing contact\n" +
+                           "5  - query if an existing contact exists\n" +
+                           "6  - to print a list of available actions.");
+        System.out.println("Choose your action: ");
     }
 }
