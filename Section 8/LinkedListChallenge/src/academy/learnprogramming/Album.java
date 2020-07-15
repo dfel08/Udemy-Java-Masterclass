57bc2a2fce3dde4a2f067e63f00e94cbf2dc92f7
dfel08 <dfellows68@gmail.com>
2020-07-15 15:31:14 -0500

    Finish InnerClassesChallenge

--- Section 8/LinkedListChallenge/src/academy/learnprogramming/Album.java
+++ Section 8/LinkedListChallenge/src/academy/learnprogramming/Album.java
@@ -32,6 +32,14 @@ public class Album {
         return null;
     }
 
+    public Song findSong(int trackNumber) {
+        int index = trackNumber - 1;
+        if((index >= 0) && (index < songs.size())) {
+            return songs.get(index);
+        }
+        return null;
+    }
+
     public boolean addToPlayList(int trackNumber, List<Song> playList) {
         int index = trackNumber - 1;
         if((index >= 0) && (index <= this.songs.size())) {
