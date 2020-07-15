c3d4715b0df9cf4abcbf07b6f113059c1a92e899
dfel08 <dfellows68@gmail.com>
2020-07-15 17:08:32 -0500

    Finish Abstraction Part 1

---
+++ Section 9/Abstraction/src/academy/learnprogramming/Parrot.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Parrot extends Bird {
+
+    public Parrot(String name) {
+        super(name);
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Flitting from branch to branch");
+    }
+}
