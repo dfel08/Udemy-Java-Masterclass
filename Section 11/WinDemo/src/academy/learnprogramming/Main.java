ac8a45dbe94b7bba8498324a7175491da2ea13c3
dfel08 <dfellows68@gmail.com>
2020-07-21 10:28:41 -0500

    Create WinDemo

--- Section 11/WinDemo/src/academy/learnprogramming/Main.java
+++ Section 11/WinDemo/src/academy/learnprogramming/Main.java
@@ -3,9 +3,6 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-
-
-        jdk.nashorn.internal.ir.Node node = null;
-        org.w3c.dom.Node anotherNode = null;
+	// write your code here
     }
 }
